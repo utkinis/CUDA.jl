d8190a1850f13458f6c553e201fe9a0c45efabfe
Ivan Utkin <iutkin@ethz.ch>
2025-02-19 18:48:09 +0100

    Flip back

--- src/memory.jl
+++ src/memory.jl
@@ -504,10 +504,10 @@ mutable struct Managed{M}
   stream::CuStream
 
   # whether there are outstanding operations that haven't been synchronized
-  captured::Bool
-  
-  # whether the memory has been captured in a way that would make the dirty bit unreliable
   dirty::Bool
+
+  # whether the memory has been captured in a way that would make the dirty bit unreliable
+  captured::Bool
 
   # guards weak dependencies
   mutex::ReentrantLock
